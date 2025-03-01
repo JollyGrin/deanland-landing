97768de26529482114f9911dfbf57d4d093f250e
JollyGrin <lol@dean.lol>
2025-03-01 19:20:40 +0100

    add projects

--- src/lib/constants-projects.ts
+++ src/lib/constants-projects.ts
@@ -4,6 +4,23 @@ export const PROJECTS = [
 		type: 'video',
 		url: 'https://jollygrin.github.io/threejs-decal',
 		src: 'https://jollygrin.github.io/threejs-decal/decal.webm'
+	},
+	{
+		title: 'threejs gallery',
+		type: 'video',
+		url: 'https://jollygrin.github.io/threejs-gallery',
+		src: 'https://jollygrin.github.io/threejs-gallery/demo.webm'
+	},
+	{
+		title: 'threejs dragdrop',
+		type: 'video',
+		url: 'https://jollygrin.github.io/threejs-dragdrop',
+		src: 'https://jollygrin.github.io/threejs-dragdrop/demo.webm'
+	},
+	{
+		title: 'threejs globe',
+		type: 'video',
+		url: 'https://globe.dean.lol',
+		src: 'https://globe.dean.lol/demo.webm'
 	}
-	// { title: 'Project 2', type: 'image', src: '/path/to/image1.jpg' },
 ];
